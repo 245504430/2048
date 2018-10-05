c65488acea81a4602d12912ffbbbc1281a13b704
245504430 <43869170+245504430@users.noreply.github.com>
2018-10-05 13:20:40 +0800

    111

--- showAnimation2048.js
+++ showAnimation2048.js
@@ -11,37 +11,37 @@ function showNumWithAnimation (i, j, randomNum) {
     // numCell.append("<img src='../20181004225158.jpg' height='100' width='100'/>");
     switch (randomNum) {
         case 2:
-            numCell.append("<img src='../photo/02100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='02100x100.jpg' height='100' width='100'/>");
             break;
         case 4:
-            numCell.append("<img src='../photo/04100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='04100x100.jpg' height='100' width='100'/>");
             break;
         case 8:
-            numCell.append("<img src='../photo/08100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='08100x100.jpg' height='100' width='100'/>");
             break;
         case 16:
-            numCell.append("<img src='../photo/16100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='16100x100.jpg' height='100' width='100'/>");
             break;
         case 32:
-            numCell.append("<img src='../photo/32100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='32100x100.jpg' height='100' width='100'/>");
             break;
         case 64:
-            numCell.append("<img src='../photo/64100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='64100x100.jpg' height='100' width='100'/>");
             break;
         case 128:
-            numCell.append("<img src='../photo/128100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='128100x100.jpg' height='100' width='100'/>");
             break;
         case 256:
-            numCell.append("<img src='../photo/256100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='256100x100.jpg' height='100' width='100'/>");
             break;
         case 512:
-            numCell.append("<img src='../photo/512100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='512100x100.jpg' height='100' width='100'/>");
             break;
         case 1024:
-            numCell.append("<img src='../photo/1024100x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='1024100x100.jpg' height='100' width='100'/>");
             break;
         case 2048:
-            numCell.append("<img src='../photo/2048x100.jpg' height='100' width='100'/>");
+            numCell.append("<img src='2048x100.jpg' height='100' width='100'/>");
             break;
 
         default:
